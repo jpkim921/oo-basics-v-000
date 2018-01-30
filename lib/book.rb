2a852339a563a3d595e1412ff736921f719655e8
Jayon Kim <jpkim921@gmail.com>
2018-01-30 01:49:10 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,11 @@
 class Book
   attr_accessor :title, :author, :page_count, :genre
   def initialize(title)
-    @title = title
+    # @title = title
   end
 
-  def turn_page
-    :page_count+=1
-  end
+  # def turn_page
+  #   :page_count+=1
+  # end
 
 end
